af3df1b6be178426ede527d5da7b821c49fda342
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2022-12-24 11:09:57 -0300

    Darkmode das páginas conectados

--- assets/js/main/darkmode.js
+++ assets/js/main/darkmode.js
@@ -1,9 +1,13 @@
 // Dark Mode
 function darkMode() {
-    const dark = document.querySelector('#btn');
-    const div  = document.querySelectorAll('.country-div')
-    const data = document.querySelectorAll('.data');
-    const list = [
+    const mydarkmode  = JSON.parse(localStorage.getItem('darkmode'));
+    const dataStorage = mydarkmode.target;
+    const icon        = document.querySelector('#icon-dark');
+    const label       = document.querySelector('#label');
+    const dark        = document.querySelector('#btn');
+    const div         = document.querySelectorAll('.country-div')
+    const data        = document.querySelectorAll('.data');
+    const list        = [
         document.querySelector('header'), 
         document.querySelector('.title'), 
         document.querySelector('.dkMode'), 
@@ -13,20 +17,40 @@ function darkMode() {
         document.querySelector('.search-filter'), 
         document.querySelector('.region-list')
     ]
+
+    if (dataStorage == 'check') {
+        icon.textContent  = 'dark_mode'
+        label.textContent = 'Dark Mode';
+        list.forEach( element => element.classList.add('dark'));
+        div.forEach( e => e.classList.add('dark'));
+        data.forEach( e => e.classList.add('dark'));
+    } else {
+        icon.textContent  = 'light_mode';
+        label.textContent = 'Light Mode';
+        list.forEach( element => element.classList.remove('dark'));
+        div.forEach( e => e.classList.remove('dark'));
+        data.forEach( e => e.classList.remove('dark'));
+    }
     
     dark.addEventListener('change', e => {
-        const icon  = document.querySelector('#icon-dark');
-        const label = document.querySelector('#label')
         if (e.target.checked) {
-            icon.textContent = 'dark_mode'
+            icon.textContent  = 'dark_mode'
             label.textContent = 'Dark Mode';
+            var target        = 'check'
         } else {
-            icon.textContent = 'light_mode';
+            icon.textContent  = 'light_mode';
             label.textContent = 'Light Mode';
+            target            = 'noCheck'
         }
 
         list.forEach( element => element.classList.toggle('dark'));
         div.forEach( e => e.classList.toggle('dark'));
         data.forEach( e => e.classList.toggle('dark'));
+
+        const objLocal = {
+            'target': target
+        }
+
+        localStorage.setItem('darkmode', JSON.stringify(objLocal))
     })
 }
--- assets/js/moreDetails/mdDarkmode.js
+++ assets/js/moreDetails/mdDarkmode.js
@@ -1,28 +1,50 @@
 // Dark Mode
 function mdDarkMode() {
-    const mddark = document.querySelector('#btn-dkMode__moreDetails');
-    const mdlist = [
+    const mydarkmode  = JSON.parse(localStorage.getItem('darkmode'));
+    const dataStorage = mydarkmode.target;
+    const mdIcon      = document.querySelector('#mdIcon-dark');
+    const mdLabel     = document.querySelector('#mdLabel');
+    const mddark      = document.querySelector('#btn-dkMode__moreDetails');
+    const mdlist      = [
         document.querySelector('header'), 
         document.querySelector('.title'), 
         document.querySelector('.dkMode'), 
         document.querySelector('.main__moreDetails'),
         document.querySelector('.btnBack'),
         document.querySelector('.info'),
-        document.querySelector('.border-Country'),
+        document.querySelector('.div__border-countries'),
+        document.querySelector('.border-Countries'),
         document.querySelector('hr')
     ]
+
+    if (dataStorage == 'check') {
+        mdIcon.textContent  = 'dark_mode';
+        mdLabel.textContent = 'Dark Mode';
+        mdlist.forEach( element => element.classList.add('dark'));
+    } else {
+        mdIcon.textContent  = 'light_mode';
+        mdLabel.textContent = 'Light Mode';
+        mdlist.forEach( element => element.classList.remove('dark'));
+    }
+
     mddark.addEventListener('change', e => {
-        const mdIcon  = document.querySelector('#mdIcon-dark');
-        const mdLabel = document.querySelector('#mdLabel');
         if (e.target.checked) {
-            mdIcon.textContent = 'dark_mode'
+            mdIcon.textContent  = 'dark_mode'
             mdLabel.textContent = 'Dark Mode';
+            var target          = 'check'                
         } else {
-            mdIcon.textContent = 'light_mode';
+            mdIcon.textContent  = 'light_mode';
             mdLabel.textContent = 'Light Mode';
+            target              = 'noCheck'            
         }
 
-        mdlist.forEach( element => element.classList.toggle('dark'));  
+        mdlist.forEach( element => element.classList.toggle('dark'));
+
+        const objLocal = {
+            'target': target
+        }
+
+        localStorage.setItem('darkmode', JSON.stringify(objLocal))
     })
 }
 
